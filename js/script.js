85a15f6402c7991b23accc145678902cf25922cd
Przemek <p.ostrowinski@gmail.com>
2017-06-29 23:13:00 +0200

    adding zoom in gallery

--- js/script.js
+++ js/script.js
@@ -17,5 +17,21 @@ $(document).ready(function () {
     //Disable link in gallery section
     $('.gallery-info').find('a').click(function (e) {
         e.preventDefault();
-    })
+    });
+    
+    //Zooming gallery image when mouse is on that photo
+    var zoomTime = '0.7s';
+    
+    $('.gallery-img').hover(function () {
+        
+        $(this).css({
+            'transition': 'background ' + zoomTime,
+            'background-size': '130% 130%'
+        });
+    }, function () {
+         $(this).css({
+            'transition': 'background ' + zoomTime,
+            'background-size': '100% 100%'
+        });
+    });
 });
